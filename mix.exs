97d38f32297b80aa1975ab4e164f601d79e35398
Nicolas Blanco <me@nicolasblan.co>
2021-07-04 14:13:40 +0300

    Use Mjml rust binding library to compile templates

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule BambooMjml.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bamboo_mjml,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :bamboo_mjml,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -29,9 +31,11 @@ defmodule BambooMjml.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:bamboo, "~> 0.8.0"},
-      {:uuid, "~> 1.1"},
-      {:phoenix, "~> 1.1", only: :test}
+      {:mjml, "~> 1.1.1"},
+      {:bamboo, ">= 2.0.0"},
+      {:phoenix, ">= 1.3.0"},
+      {:phoenix_html, "~> 2.2", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
